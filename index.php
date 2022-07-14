2d853b67dd138934684323c896b4be9d76e18722
penafortmarco <penafortmarco@gmail.com>
2022-07-13 21:43:46 -0300

    Paginación agregada

--- index.php
+++ index.php
@@ -2,8 +2,9 @@
 include_once 'admin/CRUD/bd/connection.php';
 $connection = new DbConnection();
 $dbc = $connection->Connect();
-$q = "SELECT * FROM reviews ORDER BY id DESC";
-$reviewQuery = $dbc->query($q);
+if (!$_GET) {
+    header('Location:index.php?page=1');
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,6 +14,7 @@ $reviewQuery = $dbc->query($q);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GameReview</title>
+    <link rel="stylesheet" href="/bootstrap.min.css">
     <link rel="stylesheet" href="./CSS/main.css">
     <link rel="stylesheet" href="./CSS/home/header.css">
     <link rel="stylesheet" href="./CSS/home/nav.css">
@@ -50,12 +52,31 @@ $reviewQuery = $dbc->query($q);
     </header>
     <div id="divider"></div>
     <div class="title">
-        <h1>Chequea nuestras <span>reseñas</span></h1>
+        <h1>Chequea nuestras <span>reseñas</span> más recientes</h1>
     </div>
-
-
     <div class="all-elements">
         <?php
+        //calcula la cantidad de elementos totales en la tabla.
+        $q = "SELECT * FROM reviews";
+        $reviewQuery = $dbc->query($q);
+        $totalElements = mysqli_num_rows($reviewQuery);
+        $totalView = 8; //Establece el N máximo de elementos en la vista. 
+        $pages = ceil($totalElements / $totalView);
+        //<------------------------------------------------------------------------------------->//
+        //Validaciones.
+        if ($_GET['page'] <= 0) {
+            header('Location:index.php?page=1');
+        }
+        if ($_GET['page'] > $pages) {
+            header('Location:index.php?page='.$pages);
+        }
+        //<------------------------------------------------------------------------------------->//
+        //Selecciona a los elementos mostrados en cada página.
+        $init = ($_GET['page'] - 1) * $totalView;
+        $q = "SELECT * FROM reviews ORDER BY id DESC LIMIT $init, $totalView";
+        $reviewQuery = $dbc->query($q);
+        //<------------------------------------------------------------------------------------->//
+        //Trae a la vista a todos los elementos de la db.
         while ($registerReviews = $reviewQuery->fetch_array(MYSQLI_BOTH)) {
             $spoiler = $registerReviews['spoiler'];
             if ($spoiler == 1) {
@@ -64,23 +85,63 @@ $reviewQuery = $dbc->query($q);
                 $spoiler = '<a class="no-spoiler-alert">SIN SPOILERS</a>';
             }
             echo '
-        <div class="card">
-            <div class="face front">
-                <img src="/panel/img/' . $registerReviews['image'] . '" alt="">
-                <h3>' . $registerReviews['title'] . '</h3>
-                <p>' . $spoiler . '</p>
-            </div>
-            <div class="face back">
-                <h3>' . $registerReviews['title'] . '</h3>
-                <p>' . $registerReviews['text'] . '</p>
-                <div class="link">
-                    <a><span>Publicada el ' . $registerReviews['date'] . '</span></a>
-                </div>
-            </div>
-        </div>';
+                <div class="card">
+                    <div class="face front">
+                        <img src="/panel/img/' . $registerReviews['image'] . '" alt="">
+                        <h3>' . $registerReviews['title'] . '</h3>
+                        <p>' . $spoiler . '</p>
+                    </div>
+                    <div class="face back">
+                        <h3>' . $registerReviews['title'] . '</h3>
+                        <p>' . $registerReviews['text'] . '</p>
+                            <div class="link">
+                                <a><span>Publicada el ' . $registerReviews['date'] . '</span></a>
+                            </div>
+                    </div>
+                </div>';
         }
         ?>
     </div>
+    <nav aria-label="...">
+        <ul class="pagination justify-content-center">
+            <li class="page-item
+                <?php
+                //Deshabilita el botón en caso de no haber más páginas hacia atrás.
+                echo $_GET['page'] <= 1 ? 'disabled' : ''
+                ?>
+            ">
+                <a class="page-link" href="index.php?page=<?php echo $_GET['page'] - 1 ?>">
+                    Anterior
+                </a>
+            </li>
+
+            <?php for ($i = 0; $i < $pages; $i++) : //Genera la vista de N páginas.
+            ?>
+                <li class="page-item 
+                    <?php
+                    //Selecciona la vista de la página actual.
+                    echo $_GET['page'] == $i + 1 ? 'active' : ''
+                    ?> ">
+                    <a class="page-link" href="index.php?page=<?php echo $i + 1 ?>">
+                        <?php echo $i + 1 ?>
+                    </a>
+                </li>
+            <?php endfor ?>
+
+            <li class="page-item
+                <?php
+                //Deshabilita el botón en caso de no haber más páginas hacia atrás.
+                echo $_GET['page'] >= $pages ? 'disabled' : ''
+                ?>
+            ">
+                <a class="page-link" href="index.php?page=
+                    <?php echo $_GET['page'] + 1 ?>">
+                    Siguiente
+                </a>
+            </li>
+        </ul>
+    </nav>
+
     <footer class="pie-pagina" id="footer">
         <div class="grupo-1">
             <div class="box">
@@ -106,7 +167,7 @@ $reviewQuery = $dbc->query($q);
             </div>
         </div>
         <div class="grupo-2">
-            <small>Las reviews son escritas por el WebMaster y administrador del stio.</small>
+            <small>Las reviews son escritas por el WebMaster y administrador del sitio.</small>
         </div>
     </footer>
 </body>
